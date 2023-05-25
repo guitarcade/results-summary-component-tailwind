6323fb5e8e96cd542d8dbd483dc0a45139476e48
Arik Cohen <me@arikcohen.com>
2023-05-25 13:16:33 -0400

    added custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['HankenGrotesk', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
